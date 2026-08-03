69fa1c968ed53047198c1e111e04b96dd32e54b1
andieawan <andiawan69@gmail.com>
2026-08-03 10:55:39 +0700

    Update BACKEND_URL to production endpoint

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
  * go_absen_siswa/js/config.js supaya cookie SSO yang sama bisa dibaca.
  */
 export const CONFIG = {
-  BACKEND_URL: 'GANTI_DENGAN_URL_WEB_APP_BK_SETELAH_DEPLOY',
+  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbyhWM9ZelaYX3m5m5VBR7wkshH8LikwVpb0yEUcCcqNnVgJi2SC6BIIQzMKrudsj7N5/exec',
 
   APP_NAME: 'Aplikasi Manajemen BK',
 
